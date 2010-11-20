b5c5ca7b7c3fc6231c02efe2ec1081b1bd5ea9ab
Michael Buckley <codefisher@users.noreply.github.com>
2010-11-20 12:17:43 +0000

    adding another set of new buttons, and improved add-on creation to use less global variables and support mutiple installs

--- files/loader.js
+++ files/loader.js
@@ -1,5 +1,15 @@
+// careful here, we only want to add if the are not defined
+if (!this.Cc)
+	this.Cc = Components.classes;
+if (!this.Ci)
+	this.Ci = Components.interfaces;
+// the important global objects used by the extension
 function toolbar_button_loader(parent, child){
 	for(object_name in child){
 		parent[object_name] = child[object_name];
 	}
 }
+if(!this.toolbar_button_interfaces)
+	this.toolbar_button_interfaces = {}
+if(!this.toolbar_buttons)
+	this.toolbar_buttons = {}
--- files/update.js
+++ files/update.js
@@ -1,52 +1,49 @@
+if(!this.load_toolbar_button) {
+	var load_toolbar_button = {
+		start: function() {
+			var test_ids = ["status-bar", ];
+			for (var i = 0; i < test_ids.length; i++) {
+				if (!document.getElementById(test_ids[i])) {
+					setTimeout(load_toolbar_button_start, 100);
+					return;
+				}
+			}
+			var ext_id = "{{uuid}}";
+			var version = "{{version}}";
+			var prefs = toolbar_button_interfaces.PrefBranch;
+			var app_name = toolbar_button_interfaces.XULAppInfo().name;
+			var url = "{{version_url}}" + version + "?application=" + app_name.toLowerCase();
 
-var load_toolbar_button = {
-	start: function() {
-	    var test_ids = ["status-bar", ];
-	    for (var i = 0; i < test_ids.length; i++) {
-	        if (!document.getElementById(test_ids[i])) {
-	            setTimeout(load_toolbar_button_start, 100);
-	            return;
-	        }
-	    }
-	    var ext_id = "{{uuid}}";
-	    var version = "{{version}}";
-	    var prefs = toolbar_button_interfaces.PrefBranch;
-	    var app_name = toolbar_button_interfaces.XULAppInfo().name;
-	    var url = "{{version_url}}" + version + "?application=" + app_name.toLowerCase();
+			var extensionFlagFile = toolbar_button_interfaces.Properties.get("ProfD", Ci.nsIFile);
+			extensionFlagFile.append("extensions");
+			extensionFlagFile.append(ext_id);
+			extensionFlagFile.append("installed");
 
-	    var extensionFlagFile = toolbar_button_interfaces.Properties.get("ProfD", Ci.nsIFile);
-	    extensionFlagFile.append("extensions");
-	    extensionFlagFile.append(ext_id);
-	    extensionFlagFile.append("installed");
-
-	    if(!extensionFlagFile.exists() && prefs.getCharPref("extension.tbutton.current.version") != version){
-	        prefs.setCharPref("extension.tbutton.current.version", version);
-	        load_toolbar_button.file_put_contents(extensionFlagFile,version);
-	        load_toolbar_button.load_url(url);
-	    }
-	},
-	file_put_contents: function(file,data){
-		var foStream = toolbar_button_interfaces.FileOutputStream();
-		foStream.init(file, 0x02 | 0x08 | 0x20, 0664, 0);
-		foStream.write(data, data.length);
-		foStream.close();
-	},
-	load_url: function(url) {
-	    try {
-	        var newPage = getBrowser().addTab(url);
-	        getBrowser().selectedTab = newPage;
-	    } catch (e) {
-	    	//var tabmail = document.getElementById('tabmail');
-	    	//if(tabmail) {
-	    	//	var tab = tabmail.openTab('contentTab', {contentPage: url});
-	    	//} else {
-	    		var uri = toolbar_button_interfaces.IOService.newURI(url, null, null);
-	    		toolbar_button_interfaces.ExternalProtocolService.loadUrl(uri);
-	    	//}
-	    }
+			if(!extensionFlagFile.exists() && prefs.getCharPref("extension.tbutton.current.version") != version){
+				prefs.setCharPref("extension.tbutton.current.version", version);
+				load_toolbar_button.file_put_contents(extensionFlagFile,version);
+				load_toolbar_button.load_url(url);
+			}
+			window.removeEventListener("load", load_toolbar_button.init, false);
+		},
+		file_put_contents: function(file,data){
+			var foStream = toolbar_button_interfaces.FileOutputStream();
+			foStream.init(file, 0x02 | 0x08 | 0x20, 0664, 0);
+			foStream.write(data, data.length);
+			foStream.close();
+		},
+		load_url: function(url) {
+			try {
+				var newPage = getBrowser().addTab(url);
+				getBrowser().selectedTab = newPage;
+			} catch (e) {
+				var uri = toolbar_button_interfaces.IOService.newURI(url, null, null);
+				toolbar_button_interfaces.ExternalProtocolService.loadUrl(uri);
+			}
+		},
+		init: function() {
+			setTimeout(load_toolbar_button.start, 100);
+		}
 	}
+	window.addEventListener("load", load_toolbar_button.init, false);
 }
-
-window.addEventListener("load", function() {
-		setTimeout(load_toolbar_button.start, 100);
-	}, false);
