e8735356e9b3c812ba1c31097c910bb6a9d1658f
Michael Buckley <codefisher@users.noreply.github.com>
2010-11-25 02:26:32 +0000

    new buttons and some bug fixes

---
+++ data/read-mail/browser.js
@@ -0,0 +1,13 @@
+#include initApp
+#include getAppPath
+
+readMail: function() {
+	try {
+		var shell = getShellService();
+		if (shell) {
+			shell.openApplication(Components.interfaces.nsIShellService.APPLICATION_MAIL);
+		}
+	} catch (e) {
+		toolbar_buttons.initApp("Mail");
+	}
+}
