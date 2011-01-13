a631d5fd537181e10ad7e87e5555eb9724ac2155
Michael Buckley <codefisher@users.noreply.github.com>
2011-01-13 15:44:07 +0000

    adding new button to staging and updated the cookie buttons

--- staging/tb-cookie-manager/browser.js
+++ staging/tb-cookie-manager/browser.js
@@ -1,4 +1,28 @@
-openCookieTab: function() {
-	var newPage = getBrowser().addTab("chrome://browser/content/preferences/cookies.xul");
-	getBrowser().selectedTab = newPage;
+openCookieTab: function(event) {
+	if(event.button == 1) {
+		var newPage = getBrowser().addTab("chrome://browser/content/preferences/cookies.xul");
+		getBrowser().selectedTab = newPage;
+	}
+}
+
+viewCookies: function() {
+	var wm = toolbar_buttons.interfaces.WindowMediator;
+	var win = wm.getMostRecentWindow("Browser:Cookies");
+	var eTLDService = toolbar_buttons.interfaces.EffectiveTLDService;
+
+	var eTLD;
+	var uri = window.content.document.documentURIObject;
+	try {
+		eTLD = eTLDService.getBaseDomain(uri);
+	} catch (e) {
+		// getBaseDomain will fail if the host is an IP address or is empty
+		eTLD = uri.asciiHost;
+	}
+	if (win) {
+		win.gCookiesWindow.setFilter(eTLD);
+		win.focus();
+	} else {
+		window.openDialog("chrome://browser/content/preferences/cookies.xul",
+				"Browser:Cookies", "", {filterString : eTLD});
+	}
 }
