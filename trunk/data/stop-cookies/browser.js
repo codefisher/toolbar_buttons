e004b3dd03f2ccb773753da4ebb95a665dd20524
michael <michael@f430653d-9422-40ea-b4ec-95f13f0f5d16>
2011-01-13 15:44:07 +0000

    adding new button to staging and updated the cookie buttons

--- trunk/data/stop-cookies/browser.js
+++ trunk/data/stop-cookies/browser.js
@@ -2,10 +2,8 @@ toggleCookies: function(button) {
 	toolbar_buttons.prefToggleNumber(button, 'network.cookie.cookieBehavior', [1,2,0,0]);
 }
 
-viewCookies: function(event) {
+viewCookieExceptions: function(event) {
 	if(event.button == 1) {
-		var wm = toolbar_buttons.interfaces.WindowMediator;
-		var win = wm.getMostRecentWindow("Browser:Cookies");
 		var eTLDService = toolbar_buttons.interfaces.EffectiveTLDService;
 
 		var eTLD;
@@ -16,13 +14,17 @@ viewCookies: function(event) {
 			// getBaseDomain will fail if the host is an IP address or is empty
 			eTLD = uri.asciiHost;
 		}
-		if (win) {
-			win.gCookiesWindow.setFilter(eTLD);
-			win.focus();
-		} else {
-			window.openDialog("chrome://browser/content/preferences/cookies.xul",
-					"Browser:Cookies", "", {filterString : eTLD});
-		}
+		var bundlePreferences = toolbar_buttons.interfaces.StringBundleService
+			.createBundle("chrome://browser/locale/preferences/preferences.properties");
+		var params = { blockVisible   : true,
+					   sessionVisible : true,
+					   allowVisible   : true,
+					   prefilledHost  : eTLD,
+					   permissionType : "cookie",
+					   windowTitle	: bundlePreferences.GetStringFromName("cookiepermissionstitle"),
+					   introText	  : bundlePreferences.GetStringFromName("cookiepermissionstext") };
+		window.openDialog("chrome://browser/content/preferences/permissions.xul",
+				"Browser:Permissions", "", params);
 	}
 }
 
