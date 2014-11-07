ee1799a3e127ade1c920718c401dc688e71ee515
michael <michael@f430653d-9422-40ea-b4ec-95f13f0f5d16>
2014-11-07 21:15:40 +0000

    fixing several broken buttons, and updates to text on the link button page

--- trunk/data/tb-clear-cache/browser.js
+++ trunk/data/tb-clear-cache/browser.js
@@ -1,7 +1,22 @@
 goClearBrowserCache: function() {
-    const Ci = Components.interfaces;
-    var cacheService = toolbar_buttons.interfaces.CacheService;
-    try {
-      cacheService.evictEntries(Ci.nsICache.STORE_ANYWHERE);
-    } catch(er) {}
+	var stringBundle = toolbar_buttons.interfaces.StringBundleService
+		.createBundle("chrome://{{chrome_name}}/locale/button.properties");
+	var question = stringBundle.GetStringFromName("tb-clear-cache.question");
+	var title = stringBundle.GetStringFromName("tb-clear-cache.label");
+	if(toolbar_buttons.interfaces.PromptService.confirm(null, title, question)) {
+		// copied from chrome://browser/content/sanitize.js
+	    var cache = Cc["@mozilla.org/netwerk/cache-storage-service;1"].
+	                getService(Ci.nsICacheStorageService);
+	    try {
+	      // Cache doesn't consult timespan, nor does it have the
+	      // facility for timespan-based eviction.  Wipe it.
+	      cache.clear();
+	    } catch(er) {}
+	
+	    var imageCache = Cc["@mozilla.org/image/tools;1"].
+	                     getService(Ci.imgITools).getImgCacheForDocument(null);
+	    try {
+	      imageCache.clearCache(false); // true=chrome, false=content
+	    } catch(er) {}
+    }
 }
