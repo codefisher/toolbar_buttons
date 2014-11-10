a9867ad2c44fdb82e700ec79939d4685c8ad1374
michael <michael@f430653d-9422-40ea-b4ec-95f13f0f5d16>
2014-11-10 20:27:59 +0000

    update to teh admin

--- trunk/toolbar_buttons_web/tbutton_maker/admin.py
+++ trunk/toolbar_buttons_web/tbutton_maker/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from toolbar_buttons.toolbar_buttons_web.tbutton_maker.models import Application, Button, DownloadSession
+
+class DownloadSessionAdmin(admin.ModelAdmin):
+    list_display = ['time', 'query_string']
+admin.site.register(DownloadSession, DownloadSessionAdmin)
