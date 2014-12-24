c4c13576bda52c3c580ff310713bb5e28b407d45
Michael Buckley <codefisher@users.noreply.github.com>
2014-12-24 17:53:47 +0000

    added a new home page, and updates to the release notes

--- toolbar_buttons_web/tbutton_maker/views.py
+++ toolbar_buttons_web/tbutton_maker/views.py
@@ -362,4 +362,19 @@ def list_app_buttons(request, app_name, days=30, template_name='tbutton_maker/ap
 def installed(request, mode, version):
     tbutton = get_object_or_404(ExtensionDownload, version=version, group__identifier='{03B08592-E5B4-45ff-A0BE-C1D975458688}')
     template_name = "tbutton_maker/%s.html" % mode
-    return render(request, template_name, {"tbutton": tbutton})
+    compatibility = [(settings.MOZ_APP_NAMES.get(compat.app_id), compat.min_version, compat.max_version)
+                              for compat in tbutton.compatibility.all() if compat.app_id in settings.MOZ_APP_NAMES]
+    compatibility.sort()
+    def previous_notes(release):
+        while release.previous_release and release.previous_release.release_notes:
+            release = release.previous_release
+            yield release
+    return render(request, template_name, {"tbutton": tbutton, "previous": previous_notes(tbutton), "compatibility": compatibility})
+
+def homepage(request):
+    group = DownloadGroup.objects.get(identifier='{03B08592-E5B4-45ff-A0BE-C1D975458688}')
+    tbutton = ExtensionDownload.objects.get(pk=group.latest.pk)
+    compatibility = [(settings.MOZ_APP_NAMES.get(compat.app_id), compat.min_version, compat.max_version)
+                              for compat in tbutton.compatibility.all() if compat.app_id in settings.MOZ_APP_NAMES]
+    compatibility.sort()
+    return render(request, "tbutton_maker/homepage.html", {"tbutton": tbutton, "compatibility": compatibility})
