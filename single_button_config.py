bfa8fd0bd3375221277310c439457bb70cf4bb71
codefisher <mail@codefisher.org>
2015-02-22 22:50:38 +0100

    adding/removing python scripts

---
+++ single_button_config.py
@@ -0,0 +1,19 @@
+import sys
+import json
+import os
+from collections import OrderedDict
+
+def main():
+    button = sys.argv[1]
+    config = OrderedDict()
+    config["buttons"] = [button]
+    config["extension_id"] = "%s-single@codefisher.org" % button
+    config["add_to_main_toolbar"] = [button]
+    config["current_version_pref"] = 'current.version.%s' % button
+    config["chrome_name"] = "%s-toolbar-button" % button
+    config["locale_file_prefix"] = "%s_" % button
+    with open(os.path.join("configs", "%s.json" % button), "w") as f:
+        f.write(json.dumps(config, indent=4, separators=(',', ': ')))
+        
+if __name__ == "__main__":
+    main()
