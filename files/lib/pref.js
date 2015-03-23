895262a13909b7d8bfe8146fd12bb0caef8558ee
codefisher <mail@codefisher.org>
2015-03-23 01:54:50 +0100

    adding a new pref watcher object

--- files/lib/pref.js
+++ files/lib/pref.js
@@ -94,3 +94,28 @@ loadPrefWatcher: function(pref, button_id, func) {
 		}, false);
 	}, 0);
 }
+
+
+settingWatcher: function(pref, func) {
+	this.prefs = toolbar_buttons.interfaces.PrefService.getBranch(pref);
+	this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
+	this.pref = pref;
+	this.func = func;
+
+	this.startup = function() {
+		this.prefs.addObserver("", this, false);
+	};
+
+	this.shutdown = function() {
+		this.prefs.removeObserver("", this);
+	};
+
+	this.observe = function(subject, topic, data) {
+		if (topic != "nsPref:changed") {
+			return;
+		}
+		try {
+			this.func(subject, topic, data);
+		} catch(e) {} // button might not exist
+	};
+}
