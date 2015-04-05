3c0b268e96151cedfaea9265fc21c14857e231d3
codefisher <mail@codefisher.org>
2015-04-05 18:57:02 +0200

    removing Microsoft translator since AMO said it was a security issue

--- data/translate/browser.js
+++ data/translate/browser.js
@@ -2,15 +2,14 @@ TranslatePage: function() {
 	var to = toolbar_buttons.interfaces.ExtensionPrefBranch.getCharPref("translate.lang");
 	var translator = toolbar_buttons.interfaces.ExtensionPrefBranch.getCharPref("translate.service");
 	
-	if(translator == "bing") {
+	/*if(translator == "bing") {
 		var doc = window.getBrowser().contentDocument;
 		var script = doc.createElement('script');
 		script.type = 'text/javascript';
 		script.src = 'http://labs.microsofttranslator.com/bookmarklet/default.aspx?f=js&to=' + to; 
 		doc.body.insertBefore(script, doc.body.firstChild);	
-	} else if(translator == "promt") {
-			var pto = toolbar_buttons.interfaces.ExtensionPrefBranch.getCharPref("translate.promt");
-		// this will not work yet, because we need the correct value for 'to'
+	} else*/ if(translator == "promt") {
+		var pto = toolbar_buttons.interfaces.ExtensionPrefBranch.getCharPref("translate.promt");
 		var targetURI = window.getWebNavigation().currentURI.spec;
 		var service = 'http://www.online-translator.com/siteTranslation/autolink/?direction=' + pto + '&template=General&sourceURL=' + encodeURIComponent(targetURI);
 		window.loadURI(service);
