4b0678b93970c0858682171be2e60a49d5be49d3
codefisher <mail@codefisher.org>
2015-04-07 22:48:25 +0200

    fixing open_page.js after the break of access to window and document

--- files/lib/open_page.js
+++ files/lib/open_page.js
@@ -1,20 +1,22 @@
 openPageTab: function(url, event) {
+	var win = event.target.ownerDocument.defaultView;
 	try {
 		if (event.button == 1) {
-			var browser = window.getBrowser();
+			var browser = win.getBrowser();
 			browser.selectedTab = browser.addTab(url);
 		}
 	} catch(e) {}
 }
 
-openPage: function(url) {
+openPage: function(url, event) {
+	var win = event.target.ownerDocument.defaultView;
 	var prefs = toolbar_buttons.interfaces.ExtensionPrefBranch;
 	try {
 		if (prefs.getBoolPref("always.new.tab")) {
-			var browser = window.getBrowser();
+			var browser = win.getBrowser();
 			browser.selectedTab = browser.addTab(url);
 		} else {
-			window.loadURI(url);
+			win.loadURI(url);
 		}
 	} catch(e) {
 		var uri = toolbar_buttons.interfaces.IOService.newURI(url, null, null);
@@ -23,23 +25,25 @@ openPage: function(url) {
 }
 
 LoadURL: function(url, event) {
+	var win = event.target.ownerDocument.defaultView;
 	var prefs = toolbar_buttons.interfaces.ExtensionPrefBranch;
 	if (event.button == 1 || prefs.getBoolPref("always.new.tab")) {
-		var browser = window.getBrowser();
+		var browser = win.getBrowser();
 		browser.selectedTab = browser.addTab(url);
 	} else if (event.button == 0) {
-		window.loadURI(url);
+		win.loadURI(url);
 	}
 }
 
 OpenLinkFromPref: function(name, event) {
+	var win = event.target.ownerDocument.defaultView;
 	var prefs = toolbar_buttons.interfaces.ExtensionPrefBranch;
 	var url = prefs.getCharPref(name);
 	if (event.button == 1 || prefs.getBoolPref("always.new.tab")) {
-		var browser = window.getBrowser();
+		var browser = win.getBrowser();
 		browser.selectedTab = browser.addTab(url);
 	} else if (event.button == 0) {
-		window.loadURI(url);
+		win.loadURI(url);
 	}
 }
 
