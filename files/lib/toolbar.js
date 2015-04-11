a3f268bd692f13e5642d860c5142c6ff95d1abf1
codefisher <mail@codefisher.org>
2015-04-11 22:48:14 +0200

    a more universal toggle toolbar function

--- files/lib/toolbar.js
+++ files/lib/toolbar.js
@@ -1,17 +1,21 @@
-toggleToolbar: function(aEvent, toolbar_id, force) {
+toggleToolbar: function(aEvent, toolbar_id) {
 	var doc = aEvent.target.ownerDocument;
 	var win = doc.defaultView;
 	if(!aEvent || !aEvent.originalTarget || toolbar_id != aEvent.originalTarget.parentNode.id) {
 		var toolbar = doc.getElementById(toolbar_id);
-		try {
-			// Firefox 4, mainly the bookmark toolbar button
-			win.setToolbarVisibility(toolbar, toolbar.collapsed);
-			if(force) {
-				toolbar.collapsed = !toolbar.collapsed;
+		if(toolbar.collapsed || toolbar.hidden) {
+			if (toolbar.hasAttribute('hidden')) {
+				toolbar.setAttribute('hidden', 'false');
+				toolbar.setAttribute('collapsed', 'false');
+			} else {
+				toolbar.setAttribute('collapsed', 'false');
 			}
-		} catch(e) {
-			toolbar.collapsed = !toolbar.collapsed;
-			doc.persist(toolbar_id, "collapsed");
+		} else {
+			if (toolbar.hasAttribute('hidden')) {
+				toolbar.setAttribute('hidden', 'true');
+			} else {
+				toolbar.setAttribute('collapsed', 'true');
+			}
 		}
 	}
 }
