2bab83bd7958a52904fcc32a6bc64b7de6424e3a
codefisher <mail@codefisher.org>
2015-06-06 00:56:58 +0200

    Work on integration with the panel-menu.

---
+++ data/addons/browser.js
@@ -0,0 +1,10 @@
+goOpenAddons: function(item, event) {
+	var win = event.target.ownerDocument.defaultView;
+	if(item.getAttribute('cui-areatype') == 'menu-panel') {
+		event.preventDefault();
+		event.stopPropagation();
+		win.PanelUI.showSubView('addons-panel-view', item, CustomizableUI.AREA_PANEL);
+	} else {
+		win.BrowserOpenAddonsMgr();
+	}
+}
