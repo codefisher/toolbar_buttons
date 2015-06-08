afd540174215025a6ba16addc2eb7b6e5e13f9e1
codefisher <mail@codefisher.org>
2015-06-08 15:53:49 +0200

    Changed to using the CustomizableUI to toggle toolbars, since that works better for the Bookmark Toolbar.

--- files/lib/toolbar.js
+++ files/lib/toolbar.js
@@ -3,18 +3,22 @@ toggleToolbar: function(aEvent, toolbar_id) {
 	var win = doc.defaultView;
 	if(!aEvent || !aEvent.originalTarget || toolbar_id != aEvent.originalTarget.parentNode.id) {
 		var toolbar = doc.getElementById(toolbar_id);
-		if(toolbar.collapsed || toolbar.hidden) {
-			if (toolbar.hasAttribute('hidden')) {
-				toolbar.setAttribute('hidden', 'false');
-				toolbar.setAttribute('collapsed', 'false');
+		try {
+			CustomizableUI.setToolbarVisibility(toolbar_id, (toolbar.collapsed || toolbar.hidden));
+		} catch (e) {
+			if (toolbar.collapsed || toolbar.hidden) {
+				if (toolbar.hasAttribute('hidden')) {
+					toolbar.setAttribute('hidden', 'false');
+					toolbar.setAttribute('collapsed', 'false');
+				} else {
+					toolbar.setAttribute('collapsed', 'false');
+				}
 			} else {
-				toolbar.setAttribute('collapsed', 'false');
-			}
-		} else {
-			if (toolbar.hasAttribute('hidden')) {
-				toolbar.setAttribute('hidden', 'true');
-			} else {
-				toolbar.setAttribute('collapsed', 'true');
+				if (toolbar.hasAttribute('hidden')) {
+					toolbar.setAttribute('hidden', 'true');
+				} else {
+					toolbar.setAttribute('collapsed', 'true');
+				}
 			}
 		}
 	}
