be6a4230eda1b4652053539aee7ffd78164a587f
codefisher <mail@codefisher.org>
2015-06-14 13:25:06 +0200

    Added option for how the find button works, toggle or just open.

--- data/find/browser.js
+++ data/find/browser.js
@@ -1,17 +1,26 @@
 openFindBar: function(event) {
 	var win = event.target.ownerDocument.defaultView;
-	if('gFindBar' in win) { // Firefox
-		if(win.gFindBar.hidden) {
-			win.gFindBar.open();
-		} else {
-			win.gFindBar.close();
+	var prefs = toolbar_buttons.interfaces.ExtensionPrefBranch;
+	if(prefs.getBoolPref('find-bar.toggle')) {
+		if ('gFindBar' in win) { // Firefox
+			if (win.gFindBar.hidden) {
+				win.gFindBar.onFindCommand();
+			} else {
+				win.gFindBar.close();
+			}
+		} else { // SeaMonkey
+			var findBar = win.document.getElementById("FindToolbar");
+			if (findBar.hidden) {
+				win.BrowserFind();
+			} else {
+				findBar.close();
+			}
 		}
-	} else { // SeaMonkey
-		var findBar = win.document.getElementById("FindToolbar");
-		if(findBar.hidden) {
-			win.BrowserFind();
+	} else {
+		if ('gFindBar' in win) {
+			win.gFindBar.onFindCommand();
 		} else {
-			findBar.close();
+			win.BrowserFind();
 		}
 	}
 }
