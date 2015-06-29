eeaffa7d854035de456f377c0c7ba9d50ab25107
codefisher <mail@codefisher.org>
2015-06-29 17:41:10 +0200

    Adding optional drop down to profile folder buttons.

--- data/profile-folder/button.js
+++ data/profile-folder/button.js
@@ -8,3 +8,30 @@ profileFolder: function() {
 		toolbar_buttons.interfaces.ExternalProtocolService.loadUrl(uri);
 	}
 }
+
+profileFolderSetMenu: function(event, popup) {
+	var doc = event.target.ownerDocument;
+	while(popup.firstChild) {
+		popup.removeChild(popup.firstChild);
+	}
+	var profileService = Cc["@mozilla.org/toolkit/profile-service;1"].getService(Ci.nsIToolkitProfileService);
+	var profileList = profileService.profiles;
+	while (profileList.hasMoreElements()) {
+		var profile = profileList.getNext().QueryInterface(Ci.nsIToolkitProfile);
+		var menuitem = doc.createElement('menuitem');
+		menuitem.setAttribute('label', profile.name);
+		menuitem.addEventListener('command', toolbar_buttons.openProfileFolder(profile), false);
+		popup.appendChild(menuitem);
+	}
+}
+
+openProfileFolder: function(profile) {
+	return function(event) {
+		try {
+			profile.rootDir.launch();
+		} catch (e) {
+			var uri = toolbar_buttons.interfaces.IOService.newFileURI(profile.rootDir);
+			toolbar_buttons.interfaces.ExternalProtocolService.loadUrl(uri);
+		}
+	}
+}
